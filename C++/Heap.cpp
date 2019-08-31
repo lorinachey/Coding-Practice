3d478c51dbfefcc6744826b1604066cf631afbc9
Lorin A <acheylorin@gmail.com>
2019-08-30 21:10:41 -0400

    Untabified to fix spacing issues on GitHub

--- C++/Heap.cpp
+++ C++/Heap.cpp
@@ -13,38 +13,38 @@ Author: Lorin Achey
 class Heap
 {
 public:
-	int size = 0;
-	std::vector<int> heapVector;
+    int size = 0;
+    std::vector<int> heapVector;
 
-	//TODO: Understand virtual keyword as it pertains to inheritance in MinHeap and MaxHeap classes
-	//virtual void heapify();
-	void printHeap(std::vector<int> heapVector);
+    //TODO: Understand virtual keyword as it pertains to inheritance in MinHeap and MaxHeap classes
+    //virtual void heapify();
+    void printHeap(std::vector<int> heapVector);
 };
 
 class MaxHeap : public Heap
 {
 public:
-	int size;
-	std::vector<int> heapVector;
+    int size;
+    std::vector<int> heapVector;
 
-	MaxHeap(std::vector<int> heapVector, int size) : heapVector(heapVector), size(size) {}
+    MaxHeap(std::vector<int> heapVector, int size) : heapVector(heapVector), size(size) {}
 
-	void heapify(std::vector<int> heapVector, int size, int p);
+    void heapify(std::vector<int> heapVector, int size, int p);
 
-	int removeMax(std::vector<int> heapVector);
+    int removeMax(std::vector<int> heapVector);
 };
 
 class MinHeap : public Heap
 {
 public:
-	int size;
-	std::vector<int> heapVector;
+    int size;
+    std::vector<int> heapVector;
 
-	MinHeap(std::vector<int> heapVector, int size) : heapVector(heapVector), size(size) {}
+    MinHeap(std::vector<int> heapVector, int size) : heapVector(heapVector), size(size) {}
 
-	void heapify(std::vector<int> heapVector, int size, int p);
+    void heapify(std::vector<int> heapVector, int size, int p);
 
-	int removeMin(std::vector<int> heapVector);
+    int removeMin(std::vector<int> heapVector);
 };
 
 void MinHeap::heapify(std::vector<int> heapVector, int size, int p)
@@ -59,22 +59,22 @@ void MaxHeap::heapify(std::vector<int> heapVector, int size, int p)
 
 void Heap::printHeap(std::vector<int> heapVector)
 {
-	std::cout << "Heap in Array Representation: ";
+    std::cout << "Heap in Array Representation: ";
 
-	for (auto i : heapVector)
-	{
-		std::cout << i << " ";
-	}
+    for (auto i : heapVector)
+    {
+        std::cout << i << " ";
+    }
 }
 
 int main()
 {
-	std::vector<int> heapVector = {1, 3, 0, 5, 7, 8};
-	int size = heapVector.size();
+    std::vector<int> heapVector = {1, 3, 0, 5, 7, 8};
+    int size = heapVector.size();
 
-	MinHeap* minHeap = new MinHeap(heapVector, size);
-	minHeap->heapify(minHeap->heapVector, minHeap->size, minHeap->heapVector[0]);
-	minHeap->printHeap(heapVector);
-	
-	getchar();
+    MinHeap* minHeap = new MinHeap(heapVector, size);
+    minHeap->heapify(minHeap->heapVector, minHeap->size, minHeap->heapVector[0]);
+    minHeap->printHeap(heapVector);
+    
+    getchar();
 }
