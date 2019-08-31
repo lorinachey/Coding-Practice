5882ac0867c8c1491579b06cecc1ed86e624ff4d
Lorin A <acheylorin@gmail.com>
2019-08-30 21:06:18 -0400

    Added Heap file for backup copy. Still a work in progress

---
+++ C++/Heap.cpp
@@ -0,0 +1,80 @@
+/* Heap.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+This file is about practicing setting up classes and inheritance structures in cpp. Implementing
+a min and max heap that inherit from a base heap class helps with practicing object oriented
+principles in C++ and serves as good practice for implementing data structures like trees.
+
+Author: Lorin Achey
+*/
+
+#include <iostream>
+#include <vector>
+
+class Heap
+{
+public:
+	int size = 0;
+	std::vector<int> heapVector;
+
+	//TODO: Understand virtual keyword as it pertains to inheritance in MinHeap and MaxHeap classes
+	//virtual void heapify();
+	void printHeap(std::vector<int> heapVector);
+};
+
+class MaxHeap : public Heap
+{
+public:
+	int size;
+	std::vector<int> heapVector;
+
+	MaxHeap(std::vector<int> heapVector, int size) : heapVector(heapVector), size(size) {}
+
+	void heapify(std::vector<int> heapVector, int size, int p);
+
+	int removeMax(std::vector<int> heapVector);
+};
+
+class MinHeap : public Heap
+{
+public:
+	int size;
+	std::vector<int> heapVector;
+
+	MinHeap(std::vector<int> heapVector, int size) : heapVector(heapVector), size(size) {}
+
+	void heapify(std::vector<int> heapVector, int size, int p);
+
+	int removeMin(std::vector<int> heapVector);
+};
+
+void MinHeap::heapify(std::vector<int> heapVector, int size, int p)
+{
+
+}
+
+void MaxHeap::heapify(std::vector<int> heapVector, int size, int p)
+{
+
+}
+
+void Heap::printHeap(std::vector<int> heapVector)
+{
+	std::cout << "Heap in Array Representation: ";
+
+	for (auto i : heapVector)
+	{
+		std::cout << i << " ";
+	}
+}
+
+int main()
+{
+	std::vector<int> heapVector = {1, 3, 0, 5, 7, 8};
+	int size = heapVector.size();
+
+	MinHeap* minHeap = new MinHeap(heapVector, size);
+	minHeap->heapify(minHeap->heapVector, minHeap->size, minHeap->heapVector[0]);
+	minHeap->printHeap(heapVector);
+	
+	getchar();
+}
